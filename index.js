6e003b577cbbb266abeb23aa3a95a6cbe809c6ca
rodolphonapoleon <rnapoleon@imajine.ht>
2022-01-13 01:02:29 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function App() {
+  const [data, setData] = React.useState(null);
+  const [loaded, setLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    async function getData() {
+      const response = await fetch("./books.json");
+      const json = await response.json();
+      setData(json);
+      setLoaded(true);
+    }
+    getData();
+  }, []);
+  console.log("loaded:", loaded, "data:", data);
+
+  return (
+    <>
+      <div className="container">
+        <h1>Books</h1>
+        {loaded && data.books.map((book, i) => <Book data={book} key={i} />)}
+      </div>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
